3a70b3c006a38ec60dc2e38c1e68d9cc03f12fa1
dimasm93 <software.dimas_m@icloud.com>
2023-12-14 21:40:34 +0700

    update application

--- springweb-jpa.sh
+++ springweb-jpa.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 for i in {1..10000} ; do
-    curl springweb:8080/api/nasabah/v1/list
+    curl springweb-jpa-example:8080/api/nasabah/v1/list
 done
--- springweb-jpa/src/main/java/com/maryanto/dimas/example/model/Nasabah.java
+++ springweb-jpa/src/main/java/com/maryanto/dimas/example/model/Nasabah.java
@@ -1,23 +1,30 @@
 package com.maryanto.dimas.example.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "nasabah")
-public record Nasabah(
-        @Id @GenericGenerator(name = "uuid-gen", strategy = "uuid2")
-        @GeneratedValue(generator = "uuid-gen")
-        String id,
-        @Column(name = "cif")
-        String cif,
-        @Column(name = "nama_lengkap")
-        String namaLengkap,
-        @Column(name = "tgl_lahir")
-        LocalDate tanggalLahir,
-        @Column(name = "saldo")
-        BigDecimal saldo) {
+public class Nasabah {
+    @Id
+    @GenericGenerator(name = "uuid-gen", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid-gen")
+    private String id;
+    @Column(name = "cif")
+    private String cif;
+    @Column(name = "nama_lengkap")
+    private String namaLengkap;
+    @Column(name = "tgl_lahir")
+    private LocalDate tanggalLahir;
+    @Column(name = "saldo")
+    private BigDecimal saldo;
 }
